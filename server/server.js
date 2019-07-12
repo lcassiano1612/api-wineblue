572bec15b15deee79ae105966760ea38148c2d71
Lucas de Oliveira <l.cassiano1612@gmail.com>
2019-07-11 21:18:11 -0300

    jkfkdf

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ const PORT = process.env.PORT || '3000';
 const API_URL = '/api/';
 
 const EXPRESS = require('express');
+const CORS = require('cors');
 const API_ROUTES = require('./routes');
 
 const APP = EXPRESS();
 
+APP.use(CORS());
 APP.use(EXPRESS.json());
 APP.use(API_URL, API_ROUTES);
 
