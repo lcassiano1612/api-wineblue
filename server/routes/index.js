a2b7712d56dcb90bd8bdb6c996b1258360c98f84
Lucas de Oliveira <l.cassiano1612@gmail.com>
2019-07-21 00:17:10 -0300

    sdkvjsdj

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,51 +3,61 @@ const ROUTER = EXPRESS.Router();
 
 const CONNECTION_DB = require('../db');
 
+getProductWinehouse = () => {
+  return new Promise((resolve, reject) => {
+    CONNECTION_DB.query(` SELECT 
+                            a.id,
+                            a.name,
+                            a.graduation,
+                            b.price,
+                            c.name as name_country,
+                            d.name as name_winehouse,
+                            e.path as image
+                          FROM 
+                            product a, 
+                            winehouse_product b,
+                            country c,
+                            winehouse d,
+                            image e
+                          WHERE 
+                            a.id = b.id_product 
+                            and
+                            e.id = a.id_image_thumb
+                            and
+
+                            c.id = a.id_country
+                            and 
+                            d.id = b.id_winehouse
+
+                      `, (err, result) => {
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve(result);
+      }            
+    });
+  });
+}
+
+// LIST PRODUCTS
+ROUTER.get('/products/list', async (req, res, next) => {
+  try {
+    debugger;
+    res.json(await getProductWinehouse());
+  } catch(e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+
 ROUTER.put('/package/edit', (request, response) => {
-
-
   CONNECTION_DB.query(`UPDATE package SET size = ? WHERE id = ?`, [request.query.size, request.query.id]);
-
-
 });
 
 module.exports = ROUTER;
 
-// getProductWinehouse = () => {
-//   return new Promise((resolve, reject) => {
-//     CONNECTION_DB.query(` SELECT 
-//                             a.id,
-//                             a.name,
-//                             a.graduation,
-//                             b.price,
-//                             c.name as name_country,
-//                             d.name as name_winehouse,
-//                             e.path as image
-//                           FROM 
-//                             product a, 
-//                             winehouse_product b,
-//                             country c,
-//                             winehouse d,
-//                             image e
-//                           WHERE 
-//                             a.id = b.id_product 
-//                             and
-//                             e.id = a.id_image_thumb
-//                             and
 
-//                             c.id = a.id_country
-//                             and 
-//                             d.id = b.id_winehouse
-
-//                       `, (err, result) => {
-//       if (err) {
-//         return reject(err);
-//       } else {
-//         return resolve(result);
-//       }            
-//     });
-//   });
-// }
 
 // // GET INFO PACKAGE FOR MOUNT KITS
 // ROUTER.get('/info-kits', async (req, res, next) => {
@@ -85,16 +95,3 @@ module.exports = ROUTER;
 //   //   // res.sendStatus(500);
 //   // }
 // });
-
-// //LIST PRODUCTS
-// ROUTER.get('/products/list', async (req, res, next) => {
-//   try {
-//     debugger;
-//     res.json(await getProductWinehouse());
-//   } catch(e) {
-//     console.log(e);
-//     // res.sendStatus(500);
-//   }
-// });
-
-// module.exports = ROUTER;module.exports = ROUTER;
